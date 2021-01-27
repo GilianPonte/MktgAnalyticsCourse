014df4104ec929286ea5dbe358ba09e09817380e
Jason Roos <jasonmtroos@gmail.com>
2021-01-27 15:20:37 +0100

    Updated data sets

--- R/data.R
+++ R/data.R
@@ -7,5 +7,21 @@ get_MktgAnalytics_data <- function(name = NULL) {
 		utils::data('m9_rfm', package = 'MktgAnalyticsCourse', envir = e)
 		assign('d', e$m9_rfm, envir = e)
 	}
+	if (name %in% c('module-4-tutorial')) {
+	  utils::data('m4_conjoint', package = 'MktgAnalyticsCourse', envir = e)
+	  assign('d', e$m4_conjoint, envir = e)
+	}
+	if (name %in% c('ps-1-ols')) {
+	  utils::data('ps1_ols', package = 'MktgAnalyticsCourse', envir = e)
+	  assign('d', e$ps1_ols, envir = e)
+	}
+	if (name %in% c('ps-1-logistic')) {
+	  utils::data('ps1_logistic', package = 'MktgAnalyticsCourse', envir = e)
+	  assign('d', e$ps1_logistic, envir = e)
+	}
+	if (name %in% c('ps-2-conjoint')) {
+	  utils::data('ps2_conjoint', package = 'MktgAnalyticsCourse', envir = e)
+	  assign('d', e$ps2_conjoint, envir = e)
+	}
 	e$d
 }
