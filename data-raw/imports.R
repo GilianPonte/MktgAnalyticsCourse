602d319bed7b22830d69d3bcc7c02611b716c8d4
Jason Roos <jasonmtroos@gmail.com>
2022-01-26 09:49:37 +0100

    Update and reorganization for 2021/22

---
+++ data-raw/imports.R
@@ -0,0 +1,29 @@
+m4_conjoint <- MktgAnalyticsTools::m4_conjoint_data()
+usethis::use_data(m4_conjoint, overwrite = TRUE)
+
+m8_aggregate <- MktgAnalyticsTools::m8_aggregate_data()
+usethis::use_data(m8_aggregate, overwrite = TRUE)
+
+m9_rfm <- MktgAnalyticsTools::m9_rfm_data()
+usethis::use_data(m9_rfm, overwrite = TRUE)
+
+ps1_logistic <- MktgAnalyticsTools::ps1_logit_data()
+usethis::use_data(ps1_logistic, overwrite = TRUE)
+
+ps1_ols <- MktgAnalyticsTools::ps1_ols_data()
+usethis::use_data(ps1_ols, overwrite = TRUE)
+
+ps2_conjoint <- MktgAnalyticsTools::ps2_conjoint_data()
+usethis::use_data(ps2_conjoint, overwrite = TRUE)
+
+ps4_aggregate <- MktgAnalyticsTools::ps4_aggregate_data()
+usethis::use_data(ps4_aggregate, overwrite = TRUE)
+
+rta <- MktgAnalyticsTools::rta_data()
+usethis::use_data(rta, overwrite = TRUE)
+
+rta_demo <- MktgAnalyticsTools::rta_demo_data()
+usethis::use_data(rta_demo, overwrite = TRUE)
+
+
+
--- data-raw/rfm.R
+++
@@ -1,2 +0,0 @@
-m9_rfm <- MktgAnalyticsTools::get_student_transactions(1251, 52, 1234)
-usethis::use_data(m9_rfm, overwrite = FALSE)
